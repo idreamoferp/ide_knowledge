9ab8b8d76fd3f484ea90d3ffa0b8afeae660212c
root <root@prod-01.idreamoferp.com>
2021-09-19 01:07:15 +0000

    create document type module

---
+++ document_page_type/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Document Page Type",
+    "version" : "0.1",
+    "author" : "Justin Mangini",
+    'category': 'Knowledge',
+    "depends" : ['document_page'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": "Adds document type to the document control system.",
+    'data': ['views/views.xml', 'data/data.xml'],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'certificate': '',
+    'images': [''],
+}
