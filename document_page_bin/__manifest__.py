91097202168563cb23f524c152379da58b7ea456
Justin Mangini <justin@idreamoferp.com>
2022-08-17 11:53:16 +0000

    [document_page_bin] create new module

---
+++ document_page_bin/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name" : "Document Page - Binary",
+    "version" : "0.1",
+    "author" : "Justin Mangini",
+    'category': 'Knowledge',
+    "depends" : ['document_page_type'],
+    "init_xml" : [],
+    "demo_xml" : [],
+    "description": "Adds binary attachment document type to the document control system.",
+    'data': ['views/views.xml', 'data/data.xml'],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+    'certificate': '',
+    'images': [''],
+}
